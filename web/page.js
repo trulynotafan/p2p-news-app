574f631038701ce7a55268e7426591f261c59cf7
mac <mac@macs-MacBook-Air-2.local>
2026-03-28 03:56:08 +0500

    fixed-graph-explorer-with-seperation

--- web/page.js
+++ web/page.js
@@ -5,61 +5,36 @@ statedb.admin()
 
 const { sdb } = statedb(fallback_module)
 
-console.log('p2p news app')
 const news = require('news')
 
+console.log('p2p news app')
+
 const customVault = {
-  init_blog: init_blog,
-  get_peer_blogs: get_peer_blogs,
-  get_my_posts: get_my_posts,
-  get_profile: get_profile,
-  on_update: on_update
-}
-
-async function init_blog({ username }) {
-  console.log('[customVault] init_blog:', username)
-}
-
-async function get_peer_blogs() {
-  console.log('[customVault] get_peer_blogs')
-  return new Map()
-}
-
-async function get_my_posts() {
-  console.log('[customVault] get_my_posts')
-  return []
-}
-
-async function get_profile(key) {
-  console.log('[customVault] get_profile:', key)
-  return null
-}
-
-function on_update(callback) {
-  console.log('[customVault] on_update registered')
+  init_blog: async ({ username }) => console.log('[customVault] init_blog:', username),
+  get_peer_blogs: async () => new Map(),
+  get_my_posts: async () => [],
+  get_profile: async (key) => null,
+  on_update: (callback) => console.log('[customVault] on_update registered')
 }
 
 async function init() {
   console.log('[page.js] init started')
-
   const start = await sdb.watch(handle_watch_batch)
 
-  async function handle_watch_batch(batch) {
+  function handle_watch_batch(batch) {
     console.log('[page.js] sdb watch batch:', batch)
   }
-
-  console.log('[page.js] Watch returned:', start)
 
   if (!start || start.length === 0) {
     console.error('[page.js] No active instances found for news')
     return
   }
 
-  const news_instance = start[0]
-  const { sid } = news_instance
+  const { sid } = start[0]
   console.log('[page.js] Retrieved sid for news:', sid)
 
   const app = await news({ sid, vault: customVault })
+  document.body.innerHTML = ''
   document.body.append(app)
 }
 
@@ -72,6 +47,7 @@ function fallback_module() {
         $: '',
         0: '',
         mapping: {
+          style: 'style',
           entries: 'entries',
           theme: 'theme',
           runtime: 'runtime',
@@ -87,9 +63,12 @@ function fallback_module() {
       }
     },
     drive: {
+      'style/': {},
       'entries/': {},
       'theme/': {},
-      'runtime/': {},
+      'runtime/': {
+        'viewer_data.json': { raw: '{}' }
+      },
       'mode/': {},
       'flags/': {},
       'keybinds/': {},
